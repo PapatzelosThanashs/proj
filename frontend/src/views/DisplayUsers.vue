f788745ef086bda5d4d4928e05cf7dc499378e86
sakis <papatzelosthanashs@gmail.com>
2025-05-25 21:14:37 +0300

    changes at database/ and /frontend in order to trigger builds

--- frontend/src/views/DisplayUsers.vue
+++ frontend/src/views/DisplayUsers.vue
@@ -135,7 +135,7 @@ import axios from "axios";
   width: 100%;
   display: flex;
   justify-content: flex-start; /* Align left */
-  margin-bottom: 1rem; /* Space between button and table */
+  margin-bottom: 1rem; /* Space between button and table !.*/
 }
 
 
