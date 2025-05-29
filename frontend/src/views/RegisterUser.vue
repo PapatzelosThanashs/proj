9fb255d0ea272627cb9d1a77c2e9caa08d8b4844
sakis <papatzelosthanashs@gmail.com>
2025-05-29 22:19:10 +0300

    changes at axios calls url

--- frontend/src/views/RegisterUser.vue
+++ frontend/src/views/RegisterUser.vue
@@ -139,7 +139,7 @@
 
         try {
           // Send POST request to the backend to create the new user
-          await axios.post("http://localhost:8090/api/users", this.newUser);
+          await axios.post("http://localhost/api/users", this.newUser);
           
 
           // Reset the form fields
